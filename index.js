98d62c41760c9b1142c7e266f8602af277fffcb7
StarBone <anax.tore44@gmail.com>
2024-04-19 11:33:12 +0700

    hsn

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+// Toggle active navbar menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 document.querySelector("#menu").onclick = () => {
   navbarNav.classList.toggle("active");
+};
+
+// Toggle active search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = () => {
+  searchForm.classList.toggle("active");
 };
 
 const menu = document.querySelector("#menu");
